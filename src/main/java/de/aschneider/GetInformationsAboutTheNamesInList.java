06bbfc7c944744ae6db4a1903021f5a1b3d68e71
Arthur Schneider <arthur.schneider@intellinet.de>
2018-02-26 12:01:28 +0100

    Add IntSummaryStatics class which deliver statistics of a stream
    Add Collectors joining example with delimiter, prefix and suffix.
    Also add a example where you build your own Collector

--- src/main/java/de/aschneider/GetInformationsAboutTheNamesInList.java
+++ src/main/java/de/aschneider/GetInformationsAboutTheNamesInList.java
@@ -1,59 +1,85 @@
 package de.aschneider;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import de.aschneider.model.Person;
+
 public class GetInformationsAboutTheNamesInList {
 
-    public static void main(String[] args) {
-        final List<String> names = Arrays.asList("Ben", "Maike", "Lara", "Brian", "Michael");
+	public static void main(String[] args) {
+		final List<String> names = Arrays.asList("Ben", "Maike", "Lara", "Brian", "Michael");
 
-        System.out.println(
-                "Longest name has a length of " + getNamesAsIntStream(names).max().getAsInt());
+		System.out.println("Longest name has a length of " + getNamesAsIntStream(names).max().getAsInt());
 
-        names.stream().max((a, b) -> a.length() - b.length())
-                .ifPresent(name -> System.out.println("The longest name ist " + name));
+		names.stream().max((a, b) -> a.length() - b.length())
+				.ifPresent(name -> System.out.println("The longest name ist " + name));
 
-        names.stream().reduce((a, b) -> a.length() >= b.length() ? a : b)
-                .ifPresent(name -> System.out.println("The longest name ist " + name));
+		names.stream().reduce((a, b) -> a.length() >= b.length() ? a : b)
+				.ifPresent(name -> System.out.println("The longest name ist " + name));
 
-        System.out.println(
-                "Shortest name has a length of " + getNamesAsIntStream(names).min().getAsInt());
+		// ------------------------------------------------------
 
-        names.stream().min((a, b) -> a.length() - b.length())
-                .ifPresent(name -> System.out.println("The shortest name ist " + name));
+		System.out.println("Shortest name has a length of " + getNamesAsIntStream(names).min().getAsInt());
 
-        getNamesAsIntStream(names).average().ifPresent(
-                number -> System.out.println("The avarage length of a name is " + number));
+		names.stream().min((a, b) -> a.length() - b.length())
+				.ifPresent(name -> System.out.println("The shortest name ist " + name));
 
-        names.stream()
-                .filter(name -> name.length() > getNamesAsIntStream(names).average().getAsDouble())
-                .forEach(name -> System.out.println(
-                        name + " the lenght of this name is bigger than the avarages name length"));
+		getNamesAsIntStream(names).average()
+				.ifPresent(number -> System.out.println("The avarage length of a name is " + number));
 
-        System.out.println("++++++++++++++++++");
-        System.out.println(names.stream().collect(Collectors.joining(", ")));
-        
-        System.out.println(names.stream().reduce("", (x, y) -> x + y));
+		Double averageNameLength = names.stream().collect(Collectors.averagingInt(String::length));
 
-        System.out.println(names.stream().map(String::toUpperCase).reduce("", (x, y) -> x + y));
-        System.out.println("++++++++++++++++++");
+		System.out.println("The average length of a name is " + averageNameLength);
+		// ------------------------------------------------------
 
-        System.out.println("------------------");
-        names.stream().sorted().forEach(System.out::println);
-        System.out.println("------------------");
+		names.stream().filter(name -> name.length() > getNamesAsIntStream(names).average().getAsDouble())
+				.forEach(name -> System.out
+						.println(name + " the lenght of this name is bigger than the avarages name length"));
 
-        System.out.println(
-                "Length of all names is " + names.stream().reduce("", (x, y) -> x + y).length());
+		System.out.println("++++++++++++++++++");
+		System.out.println(names.stream().collect(Collectors.joining(", ")));
 
-        System.out
-                .println(String.format("There are %s names in the list ", names.stream().count()));
+		System.out.println(names.stream().reduce("", (x, y) -> x + y));
 
-    }
+		System.out.println(names.stream().map(String::toUpperCase).reduce("", (x, y) -> x + y));
+		System.out.println("++++++++++++++++++");
 
-    public static IntStream getNamesAsIntStream(List<String> list) {
-        return list.stream().mapToInt(String::length);
-    }
+		System.out.println("------------------");
+		names.stream().sorted().forEach(System.out::println);
+		System.out.println("------------------");
+
+		//Building your own Collector
+		Collector<String, StringJoiner, String> personNameCollector =
+			    Collector.of(
+			        () -> new StringJoiner(" | "),          // supplier
+			        (j, p) -> j.add(p.toUpperCase()),  		// accumulator
+			        (j1, j2) -> j1.merge(j2),               // combiner
+			        StringJoiner::toString);                // finisher
+
+			String namesConcatenated = names
+			    .stream()
+			    .collect(personNameCollector);
+		System.out.println(namesConcatenated);
+		// ------------------------------------------------------
+
+		System.out.println("Length of all names is " + names.stream().reduce("", (x, y) -> x + y).length());
+
+		System.out.println(String.format("There are %s names in the list ", names.stream().count()));
+
+		// ------------------------------------------------------
+		IntSummaryStatistics nameSummary = names.stream().collect(Collectors.summarizingInt(String::length));
+		
+		System.out.println(nameSummary);
+
+	}
+
+	public static IntStream getNamesAsIntStream(List<String> list) {
+		return list.stream().mapToInt(String::length);
+	}
 }
--- src/main/java/de/aschneider/model/ComparePerson.java
+++ src/main/java/de/aschneider/model/ComparePerson.java
@@ -15,63 +15,72 @@ import java.util.stream.Collectors;
 
 public class ComparePerson {
 
-    public static void main(String[] args) {
-        List<Person> people = Arrays.asList(new Person("Erich", 11), new Person("Maike", 30),
-                new Person("Lisa", 30), new Person("Ben", 12), new Person("Michael", 35));
+	public static void main(String[] args) {
+		List<Person> people = Arrays.asList(new Person("Erich", 11), new Person("Maike", 30),
+				new Person("Lisa", 30), new Person("Ben", 12), new Person("Michael", 35));
 
-        List<Person> peopleAscendingAge = people.stream().sorted(Person.comparePersonAgeAscending())
-                .collect(Collectors.toList());
+		List<Person> peopleAscendingAge = people.stream().sorted(Person.comparePersonAgeAscending())
+				.collect(Collectors.toList());
 
-        List<Person> peopleDescendingAge = people.stream()
-                .sorted(Person.comparePersonAgeDescending()).collect(Collectors.toList());
+		List<Person> peopleDescendingAge = people.stream().sorted(Person.comparePersonAgeDescending())
+				.collect(Collectors.toList());
 
-        System.out.println("People ordered by age ascending:");
-        peopleAscendingAge.forEach(System.out::println);
+		System.out.println("People ordered by age ascending:");
+		peopleAscendingAge.forEach(System.out::println);
 
-        // comparing takes a Function<Person, Integer> byAge = person -> person.getAge()
-        System.out.println("People ordered by age ascending:");
-        people.stream().sorted(comparing(Person::getAge)).forEach(System.out::println);
+		// comparing takes a Function<Person, Integer> byAge = person -> person.getAge()
+		System.out.println("People ordered by age ascending:");
+		people.stream().sorted(comparing(Person::getAge)).forEach(System.out::println);
 
-        System.out.println("People ordered by age descending:");
-        peopleDescendingAge.forEach(System.out::println);
+		System.out.println("People ordered by age descending:");
+		peopleDescendingAge.forEach(System.out::println);
 
-        //--------------------------------------
-        
-        System.out.println("People ordered by name ascending:");
-        people.stream().sorted(comparing(Person::getName)).forEach(System.out::println);
+		// ------------------------------------------------------
 
-        // the method reversed is a default method of the Comparator Interface
-        System.out.println("People ordered by name descending:");
-        people.stream().sorted(comparing(Person::getName).reversed()).forEach(System.out::println);
+		System.out.println("People ordered by name ascending:");
+		people.stream().sorted(comparing(Person::getName)).forEach(System.out::println);
 
-        System.out.println("People ordered by age and the by their name:");
-        List<Person> peopleOrderedByAgeThenByName = people.stream()
-                .sorted(comparing(Person::getAge).thenComparing(Person::getName))
-                .collect(Collectors.toList());
-        peopleOrderedByAgeThenByName.forEach(System.out::println);
+		// the method reversed is a default method of the Comparator Interface
+		System.out.println("People ordered by name descending:");
+		people.stream().sorted(comparing(Person::getName).reversed()).forEach(System.out::println);
 
-        // --------------------------------------
+		System.out.println("People ordered by age and the by their name:");
+		List<Person> peopleOrderedByAgeThenByName = people.stream()
+				.sorted(comparing(Person::getAge).thenComparing(Person::getName))
+				.collect(Collectors.toList());
+		peopleOrderedByAgeThenByName.forEach(System.out::println);
 
-        people.stream().min(Person.comparePersonAgeAscending())
-                .ifPresent(p -> System.out.println("The youngest person is " + p));
+		// ------------------------------------------------------
 
-        people.stream().max(Person.comparePersonAgeAscending())
-                .ifPresent(p -> System.out.println("The eldest person is " + p));
+		people.stream().min(Person.comparePersonAgeAscending())
+				.ifPresent(p -> System.out.println("The youngest person is " + p));
 
-        // --------------------------------------
+		people.stream().max(Person.comparePersonAgeAscending())
+				.ifPresent(p -> System.out.println("The eldest person is " + p));
 
-        Map<Integer, List<String>> nameOfPeopleByAge = people.stream()
-                .collect(Collectors.groupingBy(Person::getAge, mapping(Person::getName, toList())));
-        System.out.println("People grouped by Age:" + nameOfPeopleByAge);
+		// ------------------------------------------------------
 
-        // --------------------------------------
+		Map<Integer, List<String>> nameOfPeopleByAge = people.stream()
+				.collect(Collectors.groupingBy(Person::getAge, mapping(Person::getName, toList())));
+		System.out.println("People grouped by Age:" + nameOfPeopleByAge);
 
-        Map<Character, Optional<Person>> oldestPersonOfEachLetter = people.stream().collect(
-                groupingBy(person -> person.getName().charAt(0),
-                reducing(BinaryOperator.maxBy(comparing(Person::getAge)))));
-        
-        System.out.println("Oldest person of each letter :");
-        System.out.println(oldestPersonOfEachLetter);
-    }
+		Map<Integer, String> map = people.stream().collect(
+				Collectors.toMap(Person::getAge, Person::getName, (name1, name2) -> name1 + ";" + name2));
+
+		System.out.println("People grouped by Age:" + map);
+		// ------------------------------------------------------
+
+		Map<Character, Optional<Person>> oldestPersonOfEachLetter = people.stream()
+				.collect(groupingBy(person -> person.getName().charAt(0),
+						reducing(BinaryOperator.maxBy(comparing(Person::getAge)))));
+
+		System.out.println("Oldest person of each letter :");
+		System.out.println(oldestPersonOfEachLetter);
+
+		// ------------------------------------------------------
+		String phrase = people.stream().filter(person -> person.getAge() >= 18).map(Person::getName)
+				.collect(Collectors.joining(" and ", "In Germany ", " are of legal age"));
+		System.out.println(phrase);
+	}
 
 }
